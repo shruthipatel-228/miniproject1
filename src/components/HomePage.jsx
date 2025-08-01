d33e0f550d098bbf0ee4cc489cc464d77937fffe
shruthipatel-228 <madasishruthi@gmail.com>
2025-08-01 17:52:17 +0530

    first commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,18 @@
 import './HomePage.css'
+import { useNavigate } from 'react-router-dom'
 function HomePage(){
- return(
+    let navigate=useNavigate();
+    let handleClick=()=>{
+        navigate('/preview');
+    }
+ 
+    return(
+
 <div className='main-container' >
     <div className='sub-container'>
         <h1 className='heading'>Craft your beautiful resumes</h1>
         <p className='tagline'>Build , design and download your beautiful resumes with customised templates</p>
-        <button className='startBtn'>Get Started</button>
+        <button className='startBtn' onClick={handleClick}>Generate Now </button>
 
     </div>
 </div>
